36cad62bcf7160f82f532b006b3aa75057fa680c
David Webb <webbd5050@gmail.com>
2023-09-19 17:05:06 -0400

    e5.js tests passed!

--- exercises/e5.js
+++ exercises/e5.js
@@ -1,15 +1,17 @@
-
 // EXERCISE 5
 // Return an array of all bank account holders names
 // Array example: bankAccounts in /data/data.js
 // getAllClientNames(bankAccounts) => ['Kevin', 'Spenser']
 
 export function getAllClientNames(array) {
-  // Your code goes here...
-
+  let names = []
+  for (let i = 0; i < array.length; i++) {
+    if (array[i]["name"]) {
+      names.push(array[i]["name"])
+    }
+  }
+  return names
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-5"
