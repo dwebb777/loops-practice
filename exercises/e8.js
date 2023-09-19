9ee1b189bce58191b912916de92515b3b28d8fec
David Webb <webbd5050@gmail.com>
2023-09-19 19:52:45 -0400

    e8.js has passed all tests!

--- exercises/e8.js
+++ exercises/e8.js
@@ -1,15 +1,23 @@
-
 // EXERCISE 8
 // Return an array with a bank account object with the greatest balance value
 // Array example: bankAccounts in /data/data.js
 // getClientWithGreatestBalance(bankAccounts) => [{ name: 'SomeName', balance: 32, ... }]
 
 export function getClientWithGreatestBalance(array) {
-  // Your code goes here...
-
+  let highestBalance = array[0]["balance"]
+  let highestCustomer = []
+  for (let i = 0; i < array.length; i++) {
+    if (array[i]["balance"] > highestBalance) {
+      highestBalance = array[i]["balance"]
+    }
+  }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i]["balance"] == highestBalance) {
+      highestCustomer.push(array[i])
+    }
+  }
+  return highestCustomer
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-8"
