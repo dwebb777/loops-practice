8d57fb9429f5ad09f1d4af1aab5ec5ce5de42e09
David Webb <webbd5050@gmail.com>
2023-10-08 14:31:35 -0400

    Getting easier

--- exercises/e12.js
+++ exercises/e12.js
@@ -1,16 +1,30 @@
-
 // EXERCISE 12
 // Return an array of all deposits greater than 100
 // Array example: bankAccounts in /data/data.js
 // getAllDepositsGreaterThanOneHundred(bankAccounts) => [3432, 43242.34, 23432]
 
 export function getAllDepositsGreaterThanOneHundred(array) {
-  // Your code goes here...
-
+  let deposits = []
+  let overOneHundred = []
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].deposits) {
+      deposits.push(array[i].deposits)
+    } else {
+      deposits.push(0)
+    }
+  }
+  for (let i = 0; i < deposits.length; i++) {
+    for (let j = 0; j < deposits[i].length; j++) {
+      if (deposits[i][j] > 100) {
+        overOneHundred.push(deposits[i][j])
+      } else {
+        continue
+      }
+    }
+  }
+  console.log(overOneHundred)
+  return overOneHundred
 }
-
-
-
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-12"
 // If the test has all tests passed, switch to the next exercise file
--- exercises/e13.js
+++ exercises/e13.js
@@ -1,15 +1,31 @@
-
 // EXERCISE 13
 // Return an array of bank accounts that have a sum of deposits less than 2000 or no deposits at all
 // Array example: bankAccounts in /data/data.js
 // getAllAccountsWithSumsOfDepositsLess2000(bankAccounts) => [3432, 43242.34, 23432]
 
 export function getAllAccountsWithSumsOfDepositsLess2000(array) {
-  // Your code goes here...
-
+  let deposits = []
+  let final = []
+  for (let i = 0; i < array.length; i++) {
+    //console.log(array[i].deposits)
+    let sum = 0
+    if (!array[i].deposits) {
+      deposits.push(0)
+    } else {
+      for (let j = 0; j < array[i].deposits.length; j++) {
+        sum += array[i].deposits[j]
+      }
+      deposits.push(sum)
+    }
+  }
+  for (let i = 0; i < deposits.length; i++) {
+    if (deposits[i] < 2000) {
+      final.push(array[i])
+    }
+  }
+  console.log(final)
+  return final
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-13"
