6990ea9c5371bd32b24f5ff0b642d24827b7acc9
David Webb <webbd5050@gmail.com>
2023-10-20 19:42:37 -0400

    This one was a killer!

--- exercises/e14.js
+++ exercises/e14.js
@@ -1,4 +1,3 @@
-
 // EXERCISE 14
 // The balance is supposed to equal the difference of all deposits and all withdrawals.
 // Check every bank account balance and return the array of bank accounts with a wrong balance
@@ -6,11 +5,36 @@
 // getClientsWithWrongBalance(bankAccounts) => [{ name: 'Name1', balance: 32, ... }, { name: 'Name2', balance: 3523, ... }]
 
 export function getClientsWithWrongBalance(array) {
-  // Your code goes here...
+  let wrong = []
+  for (let i = 0; i < array.length; i++) {
+    let sumOfDep = 0
+    if (!array[i].deposits) {
+      array[i].deposits = 0
+    } else {
+      //console.log(array[i].deposits)
+      for (let j = 0; j < array[i].deposits.length; j++) {
+        sumOfDep += array[i].deposits[j]
+      }
+    }
+    //console.log("Sum of deposits: ", sumOfDep)
 
+    let sumOfWith = 0
+    if (!array[i].withdrawals) {
+      array[i].withdrawals = 0
+    } else {
+      for (let m = 0; m < array[i].withdrawals.length; m++) {
+        sumOfWith += array[i].withdrawals[m]
+      }
+      //console.log("Sum of withdrawals: ", sumOfWith)
+    }
+    if (array[i].balance == sumOfDep - sumOfWith || array[i].balance == sumOfDep) {
+      continue
+    } else {
+      wrong.push(array[i])
+    }
+  }
+  return wrong
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-14"
